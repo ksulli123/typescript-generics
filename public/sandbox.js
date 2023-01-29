a9f1d7629c85fd04878aaa555155422856bdecc7
ksulli123 <ken.sullivan.web@gmail.com>
2023-01-29 23:13:52 +1000

    added some functions

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log('test');
+let greet;
+// greet = 'hello';
+greet = () => {
+    console.log('helolo');
+};
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10);
+const sum = (a, b) => {
+    return a + b;
+};
