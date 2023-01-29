31cd95d1bf81d88cde0e7c949ad4e5d31d60fcf1
ksulli123 <ken.sullivan.web@gmail.com>
2023-01-29 18:12:40 +1000

    playing with types for arrays and objects

--- sandbox.ts
+++ sandbox.ts
@@ -1,5 +1,26 @@
-const circ = (diameter: number) => {
-    return diameter * Math.PI;
+//arrays
+let names = ['luigi', 'mario', 'yoshi'];
+
+names.push('toad');
+// names.push(3);
+// names[0] = 3;
+let numbers = [0, 10, 20, 30, 40];
+
+numbers.push(25);
+// numbers.push('test');
+
+let mixed = ['ken', 5, 'james', 4, 3, true];
+mixed.push('ryu');
+mixed.push(5);
+mixed[9] = 5;
+
+//objects
+let ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30,
 }
 
-console.log(circ(5));
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '30';
